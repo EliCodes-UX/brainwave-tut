2e44fee621b30dd70c7cd01c7a248a099354169c
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-07-25 22:49:18 -0700

    added btn back to code

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
       </div>
-      <ButtonGradient />
+      {/* <Header /> */}
+      {/* <ButtonGradient /> */}
+      <Button />
     </>
   );
 }
