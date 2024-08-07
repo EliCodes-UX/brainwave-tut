a4d63b330c3e8f6411d8d3fad99134d08b9a7c96
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-08-06 20:10:26 -0700

    added grid and layout to benefits

--- src/components/Benifits.jsx
+++ src/components/Benifits.jsx
@@ -2,6 +2,7 @@ import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "../constants";
 import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
 
 const Benifits = () => {
   return (
@@ -34,6 +35,24 @@ const Benifits = () => {
                     Explore more
                   </p>
                   <Arrow />
+                </div>
+              </div>
+              {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
                 </div>
               </div>
             </div>
